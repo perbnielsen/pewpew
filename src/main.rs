845999d472d2b7511d5e31bd6a3991cf80a1b839
Per Bleshøy Nielsen <per.nielsen@productmadness.com>
2022-03-13 23:52:41 +0000

    added tank/turret asset

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,40 @@
-use bevy::{
-    core::Time,
-    input::{system::exit_on_esc_system, Input},
-    math::Vec3,
-    prelude::{App, Commands, Component, KeyCode, Query, Res, Transform},
-    DefaultPlugins,
-};
+use bevy::{input::system::exit_on_esc_system, prelude::*};
 
 fn main() {
-    println!("Hello, world!");
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_system(fps_system)
+        // .add_system(fps_system)
         .add_system(exit_on_esc_system)
-        .add_system(control_players_system)
+        .add_system(player_control_system)
         .add_system(movement_update_system)
-        .add_startup_system(create_level)
+        .add_startup_system(setup)
         .run();
 }
 
-fn fps_system(time: Res<Time>) {
-    println!("Fps: {}", 1.0f32 / time.delta_seconds());
-}
+// fn fps_system(time: Res<Time>) {
+//     println!("Fps: {}", 1.0f32 / time.delta_seconds());
+// }
 
-fn control_players_system(
-    players: Query<(&Moving, &PlayerControllerConfiguration)>,
+fn player_control_system(
+    mut players: Query<(&mut Moving, &PlayerControllerConfiguration)>,
     keyboard_input: Res<Input<KeyCode>>,
 ) {
-    for (movement, playerController) in players.iter() {
-        // compare playerControllersConfiguration to keys pressed to update the movements
-        if keyboard_input.pressed(playerController.keycode_left) {
+    for (mut movement, player_controller) in players.iter_mut() {
+        if keyboard_input.pressed(player_controller.keycode_left) {
             println!("Turn left");
+            movement.delta_yaw -= 1.0;
         }
-        if keyboard_input.pressed(playerController.keycode_right) {
+        if keyboard_input.pressed(player_controller.keycode_right) {
             println!("Turn right");
+            movement.delta_yaw += 1.0;
         }
-        if keyboard_input.pressed(playerController.keycode_forward) {
+        if keyboard_input.pressed(player_controller.keycode_forward) {
             println!("Turn forward");
+            movement.velocity += 1.0;
         }
-        if keyboard_input.pressed(playerController.keycode_reverse) {
+        if keyboard_input.pressed(player_controller.keycode_reverse) {
             println!("Turn reverse");
+            movement.velocity -= 1.0;
         }
     }
 }
@@ -51,13 +47,18 @@ struct PlayerControllerConfiguration {
     pub keycode_reverse: KeyCode,
 }
 
-impl Default for PlayerControllerConfiguration {
-    fn default() -> Self {
+impl PlayerControllerConfiguration {
+    fn new(
+        keycode_left: KeyCode,
+        keycode_right: KeyCode,
+        keycode_forward: KeyCode,
+        keycode_reverse: KeyCode,
+    ) -> Self {
         Self {
-            keycode_left: KeyCode::A,
-            keycode_right: KeyCode::S,
-            keycode_forward: KeyCode::W,
-            keycode_reverse: KeyCode::R,
+            keycode_left,
+            keycode_right,
+            keycode_forward,
+            keycode_reverse,
         }
     }
 }
@@ -65,20 +66,50 @@ impl Default for PlayerControllerConfiguration {
 #[derive(Component, Default)]
 struct Moving {
     pub velocity: f32,
+    pub delta_yaw: f32,
 }
 
-fn create_level(mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    // mut meshes: ResMut<Assets<Mesh>>,
+    // mut materials: ResMut<Assets<StandardMaterial>>,
+    assets_server: Res<AssetServer>,
+) {
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(10.0, 0.0, 0.0)
+            .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+        ..Default::default()
+    });
+    // Span a player
     commands.spawn_bundle((
         Transform::identity(),
-        PlayerControllerConfiguration::default(),
+        GlobalTransform::identity(),
+        PlayerControllerConfiguration::new(KeyCode::A, KeyCode::S, KeyCode::W, KeyCode::R),
         Moving::default(),
     ));
+
+    // let transform = Transform::from_xyz(0.0, 0.0, 0.0);
+    let mesh = assets_server.load("tank/turret.gltf#Scene0");
+    commands.spawn_scene(mesh);
+    let mesh = assets_server.load("tank/body.gltf#Scene0");
+    commands.spawn_scene(mesh);
+    // commands.spawn_bundle(PbrBundle {
+    // mesh: meshes.add(Mesh::from(shape::Box::new(5.27, 2.79, 1.02))),
+    // mesh, //assets_server.load("tank/turret.gltf#Scene0"),
+    // transform,
+    // material: materials.add(StandardMaterial {
+    //     base_color: Color::INDIGO,
+    //     perceptual_roughness: 1.0,
+    //     ..Default::default()
+    // }),
+    // ..Default::default()
+    // });
 }
 
 fn movement_update_system(mut moving_objects: Query<(&Moving, &mut Transform)>, time: Res<Time>) {
     for (movement, mut transform) in moving_objects.iter_mut() {
-        let translation =
-            transform.rotation.mul_vec3(Vec3::X) * movement.velocity * time.delta_seconds();
-        transform.translation += translation;
+        // TODO: rotate transform according to movement
+        let forward_velocity = transform.forward() * movement.velocity * time.delta_seconds();
+        transform.translation += forward_velocity;
     }
 }
