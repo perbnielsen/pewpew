899900840adb3982ef64f0504a5da16a3f4ccca4
Per Bleshøy Nielsen <per@192.168.1.137>
2022-03-31 23:29:37 +0100

    despawn bullets after time out

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use bevy::{input::system::exit_on_esc_system, prelude::*};
+use bevy_inspector_egui::WorldInspectorPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
         // .add_system(fps_system)
         .add_system(exit_on_esc_system)
         .add_system(player_control_system)
         .add_system(movement_update_system)
+        .add_system(auto_despawn_system)
         .add_startup_system(setup)
+        // .add_system(print_children_names)
         .run();
 }
 
@@ -15,13 +19,34 @@ fn main() {
 //     println!("Fps: {}", 1.0f32 / time.delta_seconds());
 // }
 
+// fn print_children_names(
+//     transforms: Query<(&Transform, &Children, &Name)>,
+//     transform_query: Query<(&Transform, &Name)>,
+// ) {
+//     for (_, children, name) in transforms.iter() {
+//         println!("{}:", name.as_str());
+//         for child in children.iter() {
+//             if let Ok((_, name)) = transform_query.get(*child) {
+//                 println!("    {}", name.as_str());
+//             } else {
+//                 println!("    Unamed child");
+//             }
+//         }
+//     }
+// }
+
 fn player_control_system(
-    mut players: Query<(&mut Moving, &PlayerControllerConfiguration, &Transform)>,
+    mut players: Query<(
+        &mut Moving,
+        &PlayerControllerConfiguration,
+        &Transform,
+        &Children,
+    )>,
     keyboard_input: Res<Input<KeyCode>>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
-    for (mut movement, player_controller, transform) in players.iter_mut() {
+    for (mut movement, player_controller, transform, _children) in players.iter_mut() {
         movement.velocity = 0.0;
         movement.delta_yaw = 0.0;
 
@@ -38,22 +63,25 @@ fn player_control_system(
             movement.velocity -= movement.speed;
         }
         if keyboard_input.just_pressed(player_controller.keycode_fire) {
-            println!("pew pew!");
-            let transform = transform.with_translation(*transform * Vec3::new(0.0, 1.517, -3.51));
-            commands
-                .spawn_bundle(PbrBundle {
-                    transform,
-                    mesh: meshes.add(Mesh::from(shape::Icosphere {
-                        radius: 0.2,
-                        subdivisions: 3,
-                    })),
-                    ..Default::default()
-                })
-                .insert(Projectile::default())
-                .insert(Moving {
-                    velocity: 20.0,
-                    ..Default::default()
-                });
+            let transform = transform.with_translation(*transform * Vec3::new(0.0, 1.52, -3.51));
+            let mut bullet = commands.spawn_bundle(PbrBundle {
+                transform,
+                mesh: meshes.add(Mesh::from(shape::Icosphere {
+                    radius: 0.2,
+                    subdivisions: 3,
+                })),
+                ..Default::default()
+            });
+            bullet.insert(Projectile::default()).insert(Moving {
+                velocity: 40.0,
+                ..Default::default()
+            });
+            let id = bullet.id();
+            let auto_despawner = AutoDespawn {
+                entity: id,
+                time_to_live: 1.0,
+            };
+            bullet.insert(auto_despawner);
         }
     }
 }
@@ -85,6 +113,25 @@ impl PlayerControllerConfiguration {
     }
 }
 
+#[derive(Debug, Component)]
+struct AutoDespawn {
+    pub entity: Entity,
+    pub time_to_live: f32,
+}
+
+fn auto_despawn_system(
+    mut commands: Commands,
+    mut auto_despawners: Query<&mut AutoDespawn>,
+    time: Res<Time>,
+) {
+    for mut auto_despawner in auto_despawners.iter_mut() {
+        auto_despawner.time_to_live -= time.delta_seconds();
+        if auto_despawner.time_to_live < 0.0 {
+            commands.entity(auto_despawner.entity).despawn_recursive();
+        }
+    }
+}
+
 #[derive(Component, Default)]
 struct Moving {
     pub speed: f32,
@@ -109,7 +156,7 @@ struct Projectile {}
 
 fn setup(mut commands: Commands, assets_server: Res<AssetServer>) {
     commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(10.0, 10.0, 0.0)
+        transform: Transform::from_xyz(30.0, 30.0, 0.0)
             .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
         ..Default::default()
     });
@@ -132,7 +179,7 @@ fn span_player(commands: &mut Commands, assets_server: &Res<AssetServer>) {
                 KeyCode::R,
                 KeyCode::Space,
             ),
-            Moving::new(5.0, 3.0),
+            Moving::new(10.0, 3.0),
         ))
         .with_children(|parent| {
             parent.spawn_scene(tank_body);
