7fcae3ee600abf88a40b9f059e7ec1c6bb48f963
Per Bleshøy Nielsen <per.b.nielsen@gmail.com>
2023-05-07 18:36:57 +1000

    feat: Add DirectionalLightShadowMap and sun light
    This commit adds the DirectionalLightShadowMap with a size of 4096. It also adds the sun light with enabled shadows, translated and rotated via Quat. In addition, it tightens the shadow bounds for better visual quality. The expressions that use these features are found in `add_sun_light` and a few places in `setup`. Finally, `spawn_floor` adds a plane on which the player stands and that has a white standard material.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod systems;
 use systems::*;
 
-use bevy::{prelude::*, window::close_on_esc};
+use bevy::{
+    pbr::{CascadeShadowConfigBuilder, DirectionalLightShadowMap},
+    prelude::*,
+    window::close_on_esc,
+};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 // use bevy_inspector_egui::{RegisterInspectable, WorldInspectorPlugin};
 use bevy_rapier3d::prelude::*;
@@ -19,6 +23,7 @@ use bevy_rapier3d::prelude::*;
 
 fn main() {
     App::new()
+        .insert_resource(DirectionalLightShadowMap { size: 4096 })
         .add_event::<LayMineEvent>()
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
@@ -42,10 +47,41 @@ fn main() {
 #[derive(Component, Default)]
 pub struct Projectile {}
 
-fn setup(mut commands: Commands, assets_server: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands,
+    assets_server: Res<AssetServer>,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<StandardMaterial>>,
+) {
     spawn_camera(&mut commands);
     spawn_player(&mut commands, &assets_server);
-    spawn_floor(&mut commands);
+    spawn_floor(&mut commands, meshes, materials);
+    add_sun_light(&mut commands);
+}
+
+fn add_sun_light(commands: &mut Commands) {
+    // directional 'sun' light
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-std::f32::consts::PI / 4.),
+            ..default()
+        },
+        // The default cascade config is designed to handle large scenes.
+        // As this example has a much smaller world, we can tighten the shadow
+        // bounds for better visual quality.
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 4.0,
+            maximum_distance: 100.0,
+            ..default()
+        }
+        .into(),
+        ..default()
+    });
 }
 
 fn spawn_camera(commands: &mut Commands) {
@@ -56,11 +92,24 @@ fn spawn_camera(commands: &mut Commands) {
     });
 }
 
-fn spawn_floor(commands: &mut Commands) {
+fn spawn_floor(
+    commands: &mut Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     commands.spawn((
         Collider::cuboid(100.0, 0.1, 100.0),
         TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)),
     ));
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(100.0).into()),
+        material: materials.add(StandardMaterial {
+            base_color: Color::WHITE,
+            perceptual_roughness: 1.0,
+            ..default()
+        }),
+        ..default()
+    });
 }
 
 fn spawn_player(commands: &mut Commands, assets_server: &Res<AssetServer>) {
